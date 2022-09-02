0e3a4a023c071147d79c2590294a4f6dc55d91c0
Thilo Michael <thilo.michael@tu-berlin.de>
2022-09-02 17:18:13 +0200

    Fixed reference to helper classes.

--- retico/__init__.py
+++ retico/__init__.py
@@ -1,6 +1,6 @@
 import retico.ius as ius
 import retico.modules as modules
-import retico._aux as aux
+import retico.helper as aux
 
 from retico_core.network import *
 from retico_core.abstract import *
