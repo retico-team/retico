a24d335f3487a98ce02f885058305a10db85e060
Thilo Michael <thilo.michael@tu-berlin.de>
2022-09-09 17:18:23 +0200

    Added text incrementalization helper.

--- retico/helper.py
+++ retico/helper.py
@@ -5,3 +5,5 @@ from retico_wav2vecasr import Wav2Vec2ASR
 from retico_speechbraintts import SpeechBrainTTS
 
 from retico_hftranslate import HFTranslate
+
+from retico_core.text import get_text_increment
