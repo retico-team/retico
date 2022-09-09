6334dc603b64f9ce36a9aa6219aca0cc26ffd3e1
Thilo Michael <thilo.michael@tu-berlin.de>
2022-09-09 17:17:58 +0200

    Fixed naming of helper (was aux)

--- retico/__init__.py
+++ retico/__init__.py
@@ -1,6 +1,6 @@
 import retico.ius as ius
 import retico.modules as modules
-import retico.helper as aux
+import retico.helper as helper
 
 from retico_core.network import *
 from retico_core.abstract import *
